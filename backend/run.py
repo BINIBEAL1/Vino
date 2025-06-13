7c6b2e2a8dcbc207ed29db640be82081305d7d23
BINIBEAL1 <bnigussie13@gmail.com>
2025-06-13 21:40:18 +0300

    Update run.py

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,8 @@
-import uvicorn
-from app import app
+# main.py
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"msg": "Vino Bingo backend running!"}
